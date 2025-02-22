b2ae7040063fdf83304dfa421fd597970e3a93fc
profvhs <rubajrafal04@gmail.com>
2025-02-22 23:43:34 +0100

    frontend dotenv variable for socket connection

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,8 +1,10 @@
 import { io, Socket } from 'socket.io-client';
 
-export const serverUrl = 'http://localhost:3000';
+const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
+
+export const serverUrl = SOCKET_SERVER_URL || 'http://localhost:3000';
 
 export const socket: Socket = io(serverUrl, {
   autoConnect: true,
-  reconnection: true,
+  reconnection: true
 });
