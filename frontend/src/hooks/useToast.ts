c63ef5b720968d985d73f43c9c1bb4e954c24b08
profvhs <rubajrafal04@gmail.com>
2025-02-22 15:59:24 +0100

    fix import, z-index for toast container

--- frontend/src/hooks/useToast.ts
+++ frontend/src/hooks/useToast.ts
@@ -1,9 +1,9 @@
-import { ToastProvider } from '../context/Toast/ToastProvider.tsx';
 import { useContext } from 'react';
 import { ToastContextType } from '../types';
+import { ToastContext } from '../context/Toast/ToastContext.ts';
 
 export const useToast = (): ToastContextType => {
-  const context = useContext(ToastProvider);
+  const context = useContext(ToastContext);
   if(!context) {
     throw new Error('useToast must be used in a ToastProvider');
   }
