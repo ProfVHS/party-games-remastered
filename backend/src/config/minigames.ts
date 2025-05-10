8ed105bab62024eaeb5a03268da7a637a61b5cce
Oliwier <worekit@gmail.com>
2025-05-10 18:45:02 +0200

    set and change turn sockets

--- backend/src/config/minigames.ts
+++ backend/src/config/minigames.ts
@@ -3,6 +3,7 @@ import { GameRoomDataType, MinigamesEnum } from '../types/roomRepositoryTypes';
 export const clickTheBombConfig: GameRoomDataType = {
   maxRounds: 5,
   currentRound: 1,
+  currentTurn: 0, // Change it to random player
   timeForTurn: 15,
   minigame: MinigamesEnum.clickTheBomb,
   currentMinigameData: {
