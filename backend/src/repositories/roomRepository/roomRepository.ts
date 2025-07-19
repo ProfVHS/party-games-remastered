66c2a952c695f49495e27ed60322fd00f4c43f56
Oliwier <worekit@gmail.com>
2025-07-19 17:32:59 +0200

    add global getKey function

--- backend/src/repositories/roomRepository/roomRepository.ts
+++ backend/src/repositories/roomRepository/roomRepository.ts
@@ -2,3 +2,7 @@ export * from './readyTable';
 export * from './playersTable';
 export * from './gameRoomTable';
 export * from './gamePlanTable';
+
+export const getKey = (roomCode: string, key: string, subKey?: string): string => {
+  return subKey ? `room:${roomCode}:${key}:${subKey}` : `room:${roomCode}:${key}`;
+};
