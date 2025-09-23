eb23e072e246992f1015337b5bfe21d0bff51f30
profvhs <rubajrafal04@gmail.com>
2025-09-23 22:37:40 +0200

    utils clean up

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -5,11 +5,6 @@ export function setSessionVariables(roomCode: string, id: string) {
   localStorage.setItem('id', id);
 }
 
-export function clearSessionVariables() {
-  localStorage.removeItem('roomCode');
-  localStorage.removeItem('id');
-}
-
 type Modifier = string | false | null | undefined;
 type ConditionalModifiers = Record<string, boolean | undefined | null>;
 type ClassName = string | undefined;
@@ -35,13 +30,6 @@ export const ClassNames = (block: string, ...args: (Modifier[] | ConditionalModi
     .map(([key]) => `${block}--${key}`);
 
   return classNames(block, ...formattedModifiers, ...conditionalClasses, className);
-};
-
-export const formatMilisecondsToTimer = (ms: number) => {
-  const seconds = Math.floor(ms / 1000);
-  const miliseconds = ms % 100;
-
-  return `${String(seconds).padStart(2, '0')}:${String(miliseconds).padStart(2, '0')}`;
 };
 
 export const generateRandomUserName = () => {
@@ -109,61 +97,4 @@ export const generateRandomUserName = () => {
   ];
 
   return `${adjectives[Math.floor(Math.random() * adjectives.length)]} ${nouns[Math.floor(Math.random() * nouns.length)]}`;
-};
-
-// Record => Object which needs to have: keys of type XYZ and values of type XYZ
-// In this case Keys NEED to be a number and values NEED to be an array of objects with...
-// ... keys named row and col which are both numbers
-export const possibleAvatarLayouts: Record<number, { row: number; col: number }[]> = {
-  // All positions comments are relative to the lobby e.g. Top means Above/Top of the lobby
-  1: [{ row: 1, col: 2 }], // Top
-  2: [
-    { row: 2, col: 1 }, // Left
-    { row: 2, col: 3 }, // Right
-  ],
-  3: [
-    { row: 1, col: 1 }, // Top left
-    { row: 1, col: 2 }, // Top
-    { row: 1, col: 3 }, // Top right
-  ],
-  4: [
-    { row: 1, col: 1 }, // Top left
-    { row: 1, col: 3 }, // Top right
-    { row: 3, col: 1 }, // Bottom left
-    { row: 3, col: 3 }, // Bottom right
-  ],
-  5: [
-    { row: 2, col: 1 }, // Left
-    { row: 1, col: 1 }, // Top left
-    { row: 1, col: 2 }, // Top
-    { row: 1, col: 3 }, // Top right
-    { row: 2, col: 3 }, // Right
-  ],
-  6: [
-    { row: 1, col: 1 }, // Top left
-    { row: 1, col: 2 }, // Top
-    { row: 1, col: 3 }, // Top right
-    { row: 3, col: 1 }, // Bottom left
-    { row: 3, col: 2 }, // Bottom
-    { row: 3, col: 3 }, // Bottom right
-  ],
-  7: [
-    { row: 1, col: 1 }, // Top left
-    { row: 1, col: 2 }, // Top
-    { row: 1, col: 3 }, // Top right
-    { row: 2, col: 1 }, // Left
-    { row: 3, col: 1 }, // Bottom left
-    { row: 3, col: 2 }, // Bottom
-    { row: 3, col: 3 }, // Bottom right
-  ],
-  8: [
-    { row: 1, col: 1 }, // Top left
-    { row: 1, col: 2 }, // Top
-    { row: 1, col: 3 }, // Top right
-    { row: 2, col: 1 }, // Left
-    { row: 2, col: 3 }, // Right
-    { row: 3, col: 1 }, // Bottom left
-    { row: 3, col: 2 }, // Bottom
-    { row: 3, col: 3 }, // Bottom right
-  ],
 };
