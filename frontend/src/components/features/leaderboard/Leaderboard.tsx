ed2a6752f5e1cfebcb7f2fa096a3738c331c426c
Oliwier Pasko <worekit@gmail.com>
2025-09-30 17:14:35 +0200

    simple leaderboard component with sorted players by score

---
+++ frontend/src/components/features/leaderboard/Leaderboard.tsx
@@ -0,0 +1,30 @@
+import { usePlayersStore } from '@stores/playersStore';
+import './Leaderboard.scss';
+import { useEffect, useState } from 'react';
+import { PlayerType } from '@shared/types';
+
+export const Leaderboard = () => {
+  const { players } = usePlayersStore();
+  const [sortedPlayers, setSortedPlayers] = useState<PlayerType[]>([]);
+
+  //TODO: Second leaderboard for game (how many points did you get, extra stats )
+
+  useEffect(() => {
+    if (!players) return;
+    setSortedPlayers([...players].sort((a, b) => Number(b.score) - Number(a.score)));
+  }, [players]);
+
+  return (
+    <div className="leaderboard">
+      <div className="leaderboard__title">Leaderboard</div>
+      <div className="leaderboard__players__container">
+        {sortedPlayers.map((player, index) => (
+          <div className={`leaderboard__player`}>
+            <div className="leaderboard__player__nickname">{index + 1 + '.' + player.nickname}</div>
+            <div className="leaderboard__player__score">{player.score}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
