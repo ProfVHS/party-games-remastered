bac126512c1d9b3c78d26462a026b5daa19dffa8
Oliwier Pasko <worekit@gmail.com>
2025-10-03 00:10:27 +0200

    test

--- shared/constants/gameRules.ts
+++ shared/constants/gameRules.ts
@@ -2,8 +2,8 @@ export const MIN_PLAYERS_TO_START = 1;
 export const MAX_PLAYERS = 8;
 
 export const CLICK_THE_BOMB_RULES = {
-  WIN: 70,
-  LOSS: -30,
-  BOMB_CLICK: 30,
+  LOSS: -50,
+  BOMB_CLICK: 15,
+  MULTIPLIER: 1.15,
   COUNTDOWN: 15, // Seconds
 };
