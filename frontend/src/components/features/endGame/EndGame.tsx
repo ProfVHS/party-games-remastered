aabfce1684ac92bdf857ce9d4167395bbfc58b21
Oliwier <worekit@gmail.com>
2025-12-10 15:39:20 +0100

    sorted players and dynamic podium layout

--- frontend/src/components/features/endGame/EndGame.tsx
+++ frontend/src/components/features/endGame/EndGame.tsx
@@ -1,9 +1,11 @@
 import './EndGame.scss';
 import { Podium } from '@components/ui/podium/Podium.tsx';
+import { sortPlayersByScore } from '@shared/utlis.ts';
 import { usePlayersStore } from '@stores/playersStore.ts';
 
 export const EndGame = () => {
   const { players } = usePlayersStore();
+  const sortedPlayers = sortPlayersByScore(players);
 
   return (
     <div className="end-game">
@@ -11,27 +13,27 @@ export const EndGame = () => {
         <span className="end-game__header__party-games">Party Games</span>
         <span className="end-game__header__title">Podium</span>
       </div>
-      {players.length < 6 ? (
+      {sortedPlayers.length < 6 ? (
         <div className="podium__container lower">
-          <Podium place={4} nickname={'Player'} score={20000} />
-          <Podium place={2} nickname={'Player'} score={200} />
-          <Podium place={1} nickname={'Player'} score={300} />
-          <Podium place={3} nickname={'Player'} score={100} />
-          <Podium place={5} nickname={'Player'} score={100} />
+          {sortedPlayers[3] && <Podium place={4} nickname={sortedPlayers[3].nickname} score={sortedPlayers[3].score} />}
+          {sortedPlayers[1] && <Podium place={2} nickname={sortedPlayers[1].nickname} score={sortedPlayers[1].score} />}
+          {sortedPlayers[0] && <Podium place={1} nickname={sortedPlayers[0].nickname} score={sortedPlayers[0].score} />}
+          {sortedPlayers[2] && <Podium place={3} nickname={sortedPlayers[2].nickname} score={sortedPlayers[2].score} />}
+          {sortedPlayers[4] && <Podium place={5} nickname={sortedPlayers[4].nickname} score={sortedPlayers[4].score} />}
         </div>
       ) : (
         <>
           <div className="podium__container">
-            <Podium place={2} nickname={'Player'} score={200} />
-            <Podium place={1} nickname={'Player'} score={300} />
-            <Podium place={3} nickname={'Player'} score={100} />
+            {sortedPlayers[1] && <Podium place={2} nickname={sortedPlayers[1].nickname} score={sortedPlayers[1].score} />}
+            {sortedPlayers[0] && <Podium place={1} nickname={sortedPlayers[0].nickname} score={sortedPlayers[0].score} />}
+            {sortedPlayers[2] && <Podium place={3} nickname={sortedPlayers[2].nickname} score={sortedPlayers[2].score} />}
           </div>
           <div className="podium__container">
-            <Podium place={7} nickname={'Player'} score={200} />
-            <Podium place={5} nickname={'Player'} score={200} />
-            <Podium place={4} nickname={'Player'} score={300} />
-            <Podium place={6} nickname={'Player'} score={100} />
-            <Podium place={8} nickname={'Player'} score={100} />
+            {sortedPlayers[6] && <Podium place={7} nickname={sortedPlayers[6].nickname} score={sortedPlayers[6].score} />}
+            {sortedPlayers[4] && <Podium place={5} nickname={sortedPlayers[4].nickname} score={sortedPlayers[4].score} />}
+            {sortedPlayers[3] && <Podium place={4} nickname={sortedPlayers[3].nickname} score={sortedPlayers[3].score} />}
+            {sortedPlayers[5] && <Podium place={6} nickname={sortedPlayers[5].nickname} score={sortedPlayers[5].score} />}
+            {sortedPlayers[7] && <Podium place={8} nickname={sortedPlayers[7].nickname} score={sortedPlayers[7].score} />}
           </div>
         </>
       )}
