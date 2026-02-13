d5234408435614004093f32025f4c6510fe570db
Oliwier <worekit@gmail.com>
2026-02-13 22:52:32 +0100

    refactor podiums

--- frontend/src/components/features/endGame/EndGame.tsx
+++ frontend/src/components/features/endGame/EndGame.tsx
@@ -2,10 +2,23 @@ import './EndGame.scss';
 import { Podium } from '@components/ui/podium/Podium.tsx';
 import { sortPlayersByScore } from '@shared/utlis.ts';
 import { usePlayersStore } from '@stores/playersStore.ts';
+import { PlayerType } from '@shared/types';
+import { useEffect, useState } from 'react';
 
 export const EndGame = () => {
-  const { players } = usePlayersStore();
+  const players = usePlayersStore((state) => state.players);
   const sortedPlayers = sortPlayersByScore(players);
+  const [firstRow, setFirstRow] = useState<PlayerType[]>([]);
+  const [secondRow, setSecondRow] = useState<PlayerType[]>([]);
+
+  useEffect(() => {
+    if (sortedPlayers.length > 5) {
+      setFirstRow(sortedPlayers.slice(0, 3));
+      setSecondRow(sortedPlayers.slice(3));
+    } else {
+      setFirstRow(sortedPlayers);
+    }
+  }, [sortedPlayers]);
 
   return (
     <div className="end-game">
@@ -13,30 +26,16 @@ export const EndGame = () => {
         <span className="end-game__header__party-games">Party Games</span>
         <span className="end-game__header__title">Podium</span>
       </div>
-      {sortedPlayers.length < 6 ? (
-        <div className="podium__container lower">
-          {sortedPlayers[3] && <Podium place={4} player={sortedPlayers[3]} />}
-          {sortedPlayers[1] && <Podium place={2} player={sortedPlayers[1]} />}
-          {sortedPlayers[0] && <Podium place={1} player={sortedPlayers[0]} />}
-          {sortedPlayers[2] && <Podium place={3} player={sortedPlayers[2]} />}
-          {sortedPlayers[4] && <Podium place={5} player={sortedPlayers[4]} />}
-        </div>
-      ) : (
-        <>
-          <div className="podium__container">
-            {sortedPlayers[1] && <Podium place={2} player={sortedPlayers[1]} />}
-            {sortedPlayers[0] && <Podium place={1} player={sortedPlayers[0]} />}
-            {sortedPlayers[2] && <Podium place={3} player={sortedPlayers[2]} />}
-          </div>
-          <div className="podium__container">
-            {sortedPlayers[6] && <Podium place={7} player={sortedPlayers[6]} />}
-            {sortedPlayers[4] && <Podium place={5} player={sortedPlayers[4]} />}
-            {sortedPlayers[3] && <Podium place={4} player={sortedPlayers[3]} />}
-            {sortedPlayers[5] && <Podium place={6} player={sortedPlayers[5]} />}
-            {sortedPlayers[7] && <Podium place={8} player={sortedPlayers[7]} />}
-          </div>
-        </>
-      )}
+      <div className="podium__container">
+        {firstRow.map((player, index) => (
+          <Podium place={index + 1} player={player} />
+        ))}
+      </div>
+      <div className="podium__container">
+        {secondRow.map((player, index) => (
+          <Podium place={index + 4} player={player} />
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/components/ui/podium/Podium.tsx
+++ frontend/src/components/ui/podium/Podium.tsx
@@ -1,8 +1,8 @@
 import './Podium.scss';
 import { createElement } from 'react';
 import { PlayerType } from '@shared/types';
-import { usePlayersStore } from '@stores/playersStore.ts';
 import { avatarList } from '@components/features/playerAvatar/avatarList.ts';
+import { ClassNames } from '@utils';
 
 type avatars = keyof typeof avatarList;
 
@@ -12,19 +12,14 @@ type PodiumProps = {
 };
 
 export const Podium = ({ place, player }: PodiumProps) => {
-  const { players } = usePlayersStore();
   const avatar = player.avatar as avatars;
 
   const placeClass = place === 1 ? 'first' : place === 2 ? 'second' : place === 3 ? 'third' : '';
-  const heights = [13, 12, 11.5, 11, 10.5, 10, 9.5, 9];
-  let height = heights[place - 1] || 10;
-
-  if (players.length < 6 && (place === 4 || place === 5)) {
-    height -= 2;
-  }
+  const heights = [13, 12, 11.5, 10, 9, 9, 8, 8];
+  const height = heights[place - 1] || 10;
 
   return (
-    <div className={`podium ${placeClass}`} style={{ height: `${height}rem` }}>
+    <div className={ClassNames('podium', [placeClass, place.toString()])} style={{ height: `${height}rem` }}>
       <div className="podium__avatar">{avatarList[avatar] && createElement(avatarList[avatar][place === 1 ? 'happy' : 'idle'])}</div>
       <div className="podium__top"></div>
       <div className="podium__base">
